488850619298ef4188965946d61392b7e71ebaf0
parmeets109 <54392833+parmeets109@users.noreply.github.com>
2019-09-24 01:24:57 +1000

    Update minesweeper_test.py
    adding the first part of code

--- minesweeper_test.py
+++ minesweeper_test.py
@@ -1,1 +1,17 @@
+from minesweeper import Minesweeper
+import unittest
 
+class MinesweeperTest(unittest.TestCase):
+    def setUp(self):
+        self.grid_size = 10
+        self.number_of_mines = 10
+        self.board_id = "1"
+        self.minesweeper = Minesweeper(self.grid_size, self.number_of_mines, self.board_id)
+
+    def tearDown(self):
+        del self.grid_size
+        del self.number_of_mines
+        del self.board_id
+        del self.minesweeper
+
+   
